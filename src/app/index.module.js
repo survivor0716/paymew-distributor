5dab59a3c846c29a8d19dac2f25da52011fa02f2
Survivor <dongchen1022@gmail.com>
2016-12-27 15:28:17 +0800

    12月27日备份

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('paymewDistributor', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'toastr']);
+
+})();
---
+++ src/app/index.route.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('paymewDistributor')
+    .config(routeConfig);
+
+  function routeConfig($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+
+})();
